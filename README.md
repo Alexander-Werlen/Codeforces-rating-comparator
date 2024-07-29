9cbae2a41a8ad49431e2cbb13f5445b8cafb8d12
Werlen Alexander <Alexwerlen@gmail.com>
2024-07-29 10:16:33 -0300

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# Codeforces Rating Comparator
 
+Compare the rating progression of multiple users in Codeforces.
+
+https://codeforces-rating-comparator.vercel.app/
--- src/components/FormSection.tsx
+++ src/components/FormSection.tsx
@@ -14,10 +14,10 @@ export const FormSection: React.FC<Props>  = ({handleSubmit, newHandle, loadingN
             
             <input type="text" id="newHandle" name="newHandle" placeholder="tourist" ref={newHandle} className="border-2 rounded h-9 p-1 border-gray-500 text-base"/>
             {!loadingNewUser && 
-            <input type="submit" value="ADD" className="ml-4 w-20 border-2 rounded p-1 cursor-pointer bg-blue-500 border-blue-500 hover:bg-blue-600 hover:border-blue-600 text-white"/>
+            <input type="submit" value="ADD" className="ml-4 w-14 sm:w-20 border-2 rounded p-1 cursor-pointer bg-blue-500 border-blue-500 hover:bg-blue-600 hover:border-blue-600 text-white"/>
             }
             {loadingNewUser && 
-            <Button disabled className="ml-4 h-9 w-20 border-2 rounded p-1 bg-blue-500 border-blue-500 text-white text-base">
+            <Button disabled className="ml-4 h-9 w-14 sm:w-20 border-2 rounded p-1 bg-blue-500 border-blue-500 text-white text-base">
             ADD
             <Loader2 className="ml-2 h-4 w-4 animate-spin" />
             </Button>
