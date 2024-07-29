e3c29fd14da9f83f010e34ead405c99bdcd01335
Werlen Alexander <Alexwerlen@gmail.com>
2024-07-29 09:54:34 -0300

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <>
   <App /> 
+  <Analytics />
+  </>
 )
