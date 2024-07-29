5e2c7e9db6db113b11ad79fa00f92c2dc28acf3d
Werlen Alexander <Alexwerlen@gmail.com>
2024-07-29 09:56:31 -0300

    added vercel speedinsights

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
   <App /> 
   <Analytics />
+  <SpeedInsights />
   </>
 )
